b13128ad2a6f375eece3f3f54b97cee36ebdc07f
catreenaa <dgegora.ekaterina@gmail.com>
2024-11-27 11:08:54 +0300

    KZ11.1

---
+++ KZ11.1/KZ11.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        using (StreamWriter writer=new StreamWriter("f.txt")) //создание файла
+        {   
+            writer.WriteLine("x sin(x)"); //Запись заголовка
+            for (double x=0;x<=1;x+=0.1) //запись значений
+            {               
+                writer.WriteLine($"{x:F1} {Math.Sin(x):F4}"); //форматирование строки и запись в файл
+            }
+        }
+        Console.WriteLine("Таблица записана в файл f.txt");
+        Console.ReadLine(); //Ожидание нажатия клавиши для завершения программы
+    }
+}
