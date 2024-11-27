b68a33e3583b9dd5803a7344ea98c3b732848060
catreenaa <dgegora.ekaterina@gmail.com>
2024-11-27 10:56:44 +0300

    Добавлено задание КЗ 7.1

---
+++ KZZ7.1/KZZ7.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Введите число: ");
+            string input=Console.ReadLine();
+            int result=ParseInput(input);
+            Console.WriteLine($"Вы ввели число: {result}");
+        }
+        catch(OverflowException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+    public static int ParseInput(string input)
+    {
+        if (int.TryParse(input,out int number))
+        {
+            return number;
+        }
+        else
+        {
+            if (long.TryParse(input,out long longNumber))
+            {
+                if (longNumber>int.MaxValue)
+                    throw new OverflowException("Введено слишком большое число");
+                if (longNumber<int.MinValue)
+                    throw new OverflowException("Введено слишком маленькое число");
+            }
+            throw new FormatException("Введено не число");
+        }
+    }
+}
---
+++ KZZ7.1/KZZ7.1/ProgramTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class ProgramTests
+{
+    [Test]
+    public void ParseInput_ValidNumber_ReturnsNumber()
+    {
+        string input="123";
+        int result=Program.ParseInput(input);
+        Assert.That(result,Is.EqualTo(123));
+    }
+
+    [Test]
+    public void ParseInput_TooLargeNumber_ThrowsOverflowException()
+    {
+        string input="3000000000";  //Это число больше, чем int.MaxValue
+        var ex=Assert.Throws<OverflowException>(()=>Program.ParseInput(input));
+        Assert.That(ex.Message,Is.EqualTo("Введено слишком большое число"));
+    }
+
+    [Test]
+    public void ParseInput_TooSmallNumber_ThrowsOverflowException()
+    {
+        string input="-3000000000";  //Это число меньше, чем int.MinValue
+        var ex=Assert.Throws<OverflowException>(()=>Program.ParseInput(input));
+        Assert.That(ex.Message,Is.EqualTo("Введено слишком маленькое число"));
+    }
+
+    [Test]
+    public void ParseInput_NotANumber_ThrowsFormatException()
+    {
+        string input = "abc";  // Это не число
+        var ex = Assert.Throws<FormatException>(() => Program.ParseInput(input));
+        Assert.That(ex.Message, Is.EqualTo("Введено не число."));
+    }
+
+    [Test]
+    public void ParseInput_ValidNegativeNumber_ReturnsNumber()
+    {
+        string input = "-123";
+        int result = Program.ParseInput(input);
+        Assert.That(result, Is.EqualTo(-123));
+    }
+}
